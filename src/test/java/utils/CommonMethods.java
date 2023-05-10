05a104e03fc7d7a637254e20c8419cf279175d03
Khan Afsar <khanafsar456@gmail.com>
2023-05-10 12:54:45 -0700

    Final changes done

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -23,10 +24,6 @@ public class CommonMethods {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
                 break;
             default:
                 throw new RuntimeException("Invalid browser name");
--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String CONFIG_PATH = "src/test/resources/config/Config.properties";
     public static final String CSVFILE_PATH = "src/test/resources/TestData/Validation_results.csv";
     public static final String CSVFILE_PATH01 = "src/test/resources/TestData/Validation_results01.csv";
-    public static final int IMPLICIT_WAIT = 30;
+    public static final int IMPLICIT_WAIT = 90;
     public static final int EXPLICIT_WAIT = 90;
 
 }
